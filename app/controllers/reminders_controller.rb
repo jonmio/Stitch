2f47fb8b08c7337e5d18bc844094d64e128bd3f7
Jon Mio <jonathan.mio@uwaterloo.ca>
2017-02-11 19:01:54 -0500

    change queqry method in index.html.erb for reminders

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -2,8 +2,6 @@ class RemindersController < ApplicationController
   before_action :ensure_logged_in
 
   def index
-    @type = current_user.reminder_platform
-    @types = ['Email', "Twitter", "Text"]
     @reminders = Reminder.where(user_id:current_user.id).order(time_since_last_contact: :desc)
   end
 end
